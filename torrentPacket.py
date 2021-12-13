1735534f7c2c8935680d4955a1f1209f055e229f
VoidxHoshi <55886143+VoidxHoshi@users.noreply.github.com>
2021-12-13 12:46:32 +0800

    Update torrentPacket.py

--- torrentPacket.py
+++ torrentPacket.py
@@ -10,48 +10,49 @@ import sys
 import pickle
 from torrentData import torrentData
 
+
 class torrentPacket:
-	def __init__(self, sender_id, type, info, data):
-		if len(data) > torrentData.CHUNK_SIZE:
-        		raise Exception("Data too large (max 512 char): ", len(data))
+    def __init__(self, sender_id, type, info, data):
+        if len(data) > torrentData.CHUNK_SIZE:
+            raise Exception("Data too large (max 512 char): ", len(data))
 
-		self.id = sender_id
-		self.type = type
-		self.info = info	# torrentData instance
-		self.data = data
+        self.id = sender_id
+        self.type = type
+        self.info = info  # torrentData instance
+        self.data = data
 
-	def get_tcp_data(self):
-		return pickle.dumps(self)
+    def get_tcp_data(self):
+        return pickle.dumps(self)
 
-	@staticmethod
-	def create_info(sender_id, info):
-		return torrentPacket(sender_id, 0, info, [])
+    @staticmethod
+    def create_info(sender_id, info, upload, download):
+        return torrentPacket(sender_id, 0, info, [upload, download])
 
-	@staticmethod
-	def create_eot(sender_id, rec): # gives state before exiting for logging
-		# send empty torrentData (or send 0?)
-		return torrentPacket(sender_id, 1, torrentData(), [rec])
+    @staticmethod
+    def create_eot(sender_id, rec):  # gives state before exiting for logging
+        # send empty torrentData (or send 0?)
+        return torrentPacket(sender_id, 1, torrentData(), [rec])
 
-	@staticmethod
-	def create_req(sender_id, filename, chunk):
-		# create fake network data with only 1 file and 1 chunk to overload info field
-		# informs receiver of what to send back
-		reqData = torrentData()
-		reqData.addPeer(sender_id, 0, 0)
-		reqData.addFile(filename, (chunk+1)*torrentData.CHUNK_SIZE)
-		reqData.peerAquireFileChunk(sender_id, filename, chunk)
-		return torrentPacket(sender_id, 2, reqData, [])
+    @staticmethod
+    def create_req(sender_id, hashname, filename, chunk, upload, download):
+        # create fake network data with only 1 file and 1 chunk to overload info field
+        # informs receiver of what to send back
+        reqData = torrentData()
+        reqData.addPeer(sender_id, 0, 0, upload, download)
+        reqData.addFile(hashname, filename, (chunk + 1) * torrentData.CHUNK_SIZE)
+        reqData.peerAquireFileChunk(sender_id, hashname, chunk)
+        return torrentPacket(sender_id, 2, reqData, [])
 
-	@staticmethod
-	def create_data(sender_id, filename, chunk, data):
-		# create fake network data with only 1 file and 1 chunk to overload info field
-		# informs receiver what the data pertains to
-		reqData = torrentData()
-		reqData.addPeer(sender_id, 0, 0)
-		reqData.addFile(filename, (chunk+1)*torrentData.CHUNK_SIZE)
-		reqData.peerAquireFileChunk(sender_id, filename, chunk)
-		return torrentPacket(sender_id, 3, reqData, data)
+    @staticmethod
+    def create_data(sender_id, hashname, filename, chunk, data, upload, download):
+        # create fake network data with only 1 file and 1 chunk to overload info field
+        # informs receiver what the data pertains to
+        reqData = torrentData()
+        reqData.addPeer(sender_id, 0, 0, upload, download)
+        reqData.addFile(hashname, filename, (chunk + 1) * torrentData.CHUNK_SIZE)
+        reqData.peerAquireFileChunk(sender_id, hashname, chunk)
+        return torrentPacket(sender_id, 3, reqData, data)
 
-	@staticmethod
-	def parse_tcp_data(TCPdata):
-		return pickle.loads(TCPdata)
+    @staticmethod
+    def parse_tcp_data(TCPdata):
+        return pickle.loads(TCPdata)
